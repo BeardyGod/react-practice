292daf24d1c6c7ed6e88ee6ff5f5bf95fe9609a6
BeardyGod <beardygod@gmail.com>
2020-10-16 20:08:06 +0530

    Updates

---
+++ precache-manifest.ba48bf2b0289c721e66a68b57f05ed42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ebbe8096c14759665b9f7687407eabe",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "8f75d3bdbe8fcc3a9a7c",
+    "url": "/react-practice/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "765b32e5b532e97572c4",
+    "url": "/react-practice/static/js/2.49932d43.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-practice/static/js/2.49932d43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f75d3bdbe8fcc3a9a7c",
+    "url": "/react-practice/static/js/main.e326db30.chunk.js"
+  },
+  {
+    "revision": "4954ff841917450cba67",
+    "url": "/react-practice/static/js/runtime-main.4cda23c3.js"
+  }
+]);
